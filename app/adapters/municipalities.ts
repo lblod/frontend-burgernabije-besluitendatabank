dbf0033bc4ccf751f3272547baf00a9695578b8a
Denperidge <denperidge@gmail.com>
2023-02-01 22:15:18 +0100

    Successful Vlaanderen API implementation

--- app/adapters/municipalities.ts
+++
@@ -1,9 +0,0 @@
-import DS from "ember-data";
-
-export default DS.JSONAPIAdapter.extend({
-  host: "https://api.basisregisters.vlaanderen.be",
-  namespace: "v2/gemeenten",
-  pathForType: function (type: any) {
-    return "";
-  },
-});
