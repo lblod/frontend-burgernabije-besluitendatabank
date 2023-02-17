8bbf805b45c1bdb4618671957fd56d4b3a86e971
snenenenenenene <sennebels@gmail.com>
2023-02-17 11:03:31 +0100

    feat: :truck: creating municipality route

--- app/router.js
+++ app/router.js
@@ -9,4 +9,5 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route("home", { path: "/" });
   this.route("detail", { path: "/agendapunt/:id" });
+  this.route("municipality", { path: "/municipality/:municipality" });
 });
