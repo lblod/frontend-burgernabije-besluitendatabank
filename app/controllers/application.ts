73e98d35f816bdd326bd17772c9a0c11ab4eb26f
snenenenenenene <sennebels@gmail.com>
2023-03-06 14:26:41 +0100

    feat: :package: attempting to add VO navbar

---
+++ app/controllers/application.ts
@@ -0,0 +1,31 @@
+import Controller from "@ember/controller";
+import config from "frontend-burgernabije-besluitendatabank/config/environment";
+
+export default class ApplicationController extends Controller {
+  VO_HEADER_WIDGET_URL =
+    "https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/7eed3599-96aa-435b-bc43-fe13d2fc0531";
+  VO_FOOTER_WIDGET_URL =
+    "https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/7eed3599-96aa-435b-bc43-fe13d2fc0531";
+
+  get hasExternallySourcedHeader() {
+    try {
+      new URL(
+        "https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/7eed3599-96aa-435b-bc43-fe13d2fc0531"
+      );
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  get hasExternallySourcedFooter() {
+    try {
+      new URL(
+        "https://tni.widgets.burgerprofiel.dev-vlaanderen.be/api/v1/widget/7eed3599-96aa-435b-bc43-fe13d2fc0531"
+      );
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+}
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,12 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    "@appuniversum/ember-appuniversum": {
+      disableWormholeElement: true,
+    },
+    "ember-cli-babel": {
+      includePolyfill: true,
+    },
     autoImport: {
       webpack: {
         // node: {
@@ -11,22 +17,10 @@ module.exports = function (defaults) {
         // },
       },
     },
-
-    // Add options here
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  app.import("node_modules/url-polyfill/url-polyfill.min.js");
+  app.import("node_modules/@govflanders/vl-widget-polyfill/dist/index.js");
+  app.import("node_modules/@govflanders/vl-widget-client/dist/index.js");
 
   return app.toTree();
 };
