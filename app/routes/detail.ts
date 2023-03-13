8190b8db8bfcae4b8d5afaabc3ea47c849d1a0f0
snenenenenenene <sennebels@gmail.com>
2023-03-13 09:58:59 +0100

    feat: :sparkles: added conditionals as to make the application not crash + added axios to test

--- app/routes/detail.ts
+++ app/routes/detail.ts
@@ -1,11 +1,20 @@
 import Store from "@ember-data/store";
 import Route from "@ember/routing/route";
 import { service } from "@ember/service";
+import axios from "axios";
 
 export default class DetailRoute extends Route {
   @service declare store: Store;
-  model(params: any) {
+  async model(params: any) {
     const { id } = params;
-    return this.store.findRecord("items", id);
+    return await axios
+      .get(
+        `https://burgernabije-besluitendatabank-dev.s.redhost.be/agenda-items?filter[id]=${id}`
+      )
+      .then((resp) => {
+        console.log(resp.data.data[0]);
+        return resp.data.data[0];
+      });
+    // return this.store.findRecord("agenda_items", id);
   }
 }
