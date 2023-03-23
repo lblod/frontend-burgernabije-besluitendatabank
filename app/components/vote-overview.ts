708c2247f3ccc4513754ecac11bd4aee15a1729e
snenenenenenene <sennebels@gmail.com>
2023-03-23 14:34:56 +0100

    refactor: :recycle: refactored to use pie chart

--- app/components/vote-overview.ts
+++ app/components/vote-overview.ts
@@ -5,10 +5,13 @@ interface ArgsInterface {}
 
 export default class VoteOverview extends Component<ArgsInterface> {
   @tracked data = {
-    columns: [["data", 91.4]],
-    type: "gauge",
+    columns: [
+      ["Voor", 30],
+      ["Tegen", 120],
+      ["Onthouden", 120],
+    ],
+    type: "pie",
   };
-
   // the three color levels for the percentage values
   color = {
     pattern: ["#FF0000", "#F97600", "#F6C600", "#60B044"],
