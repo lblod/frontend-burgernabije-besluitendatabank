864bf713e5e809b4060884cdf137daa53c6b1a6f
snenenenenenene <sennebels@gmail.com>
2023-03-31 13:00:28 +0200

    fix: :bug: added total numbers instead of percentages

--- app/components/vote-overview.ts
+++ app/components/vote-overview.ts
@@ -1,30 +1,46 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
+import d3 from "d3";
 
 interface ArgsInterface {}
 
 export default class VoteOverview extends Component<ArgsInterface> {
+  @tracked pie = {
+    label: {
+      format: function (value, ratio, id) {
+        return value;
+      },
+    },
+  };
+
+  @tracked tooltip = {
+    format: {
+      title: (d) => {
+        return "Data " + d;
+      },
+      value: (value, ratio, id) => {
+        var format = id === "data1" ? d3.format(",") : d3.format("$");
+        return format(value);
+      },
+    },
+  };
+
   @tracked data = {
     columns: [
-      ["Voor", 30],
-      ["Tegen", 120],
-      ["Onthouden", 120],
+      ["Voor", 80],
+      ["Tegen", 40],
+      ["Onthouden", 10],
     ],
     type: "pie",
   };
   // the three color levels for the percentage values
   color = {
-    pattern: ["#FF0000", "#F97600", "#F6C600", "#60B044"],
+    pattern: ["#B2CCEF", "#5990DE", "#0055CC"],
     threshold: {
       values: [30, 60, 90, 100],
     },
   };
 
-  size = {
-    height: 180,
-  };
-
   // chart title
   title = { text: "Openbare stemming" };
-  padding = { top: 20 };
 }
