47fd2f71ae6e4060445ad3dac13824c0be823d62
snenenenenenene <sennebels@gmail.com>
2023-04-07 13:21:48 +0200

    fix: :lipstick: fixed styling pt 2

--- app/routes/municipality.ts
+++ app/routes/municipality.ts
@@ -1,6 +1,9 @@
 import Store from "@ember-data/store";
 import Route from "@ember/routing/route";
 import { service } from "@ember/service";
+import axios from "axios";
+import Ember from "ember";
+import { getAllAgendaItemsQuery } from "frontend-burgernabije-besluitendatabank/utils/sparqlQueries";
 
 export default class MunicipalityRoute extends Route {
   @service declare store: Store;
@@ -14,15 +17,32 @@ export default class MunicipalityRoute extends Route {
   async model(params: any) {
     const { municipality, page } = params;
 
-    // const data = await Ember.RSVP.hash({
-    //   agenda_items: this.store.query("agenda-items", {
-    //     municipality: municipality,
-    //     page: page,
-    //     limit: 3,
-    //   }),
-    //   title: municipality,
-    // });
+    const data = await Ember.RSVP.hash({
+      agenda_items: axios
+        .get(
+          "https://qa.centrale-vindplaats.lblod.info/sparql?query=" +
+            encodeURIComponent(
+              getAllAgendaItemsQuery({
+                municipality: municipality,
+                offset: page * 3,
+              })
+            ),
+          {
+            headers: {
+              Accept: "application/sparql-results+json",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response.data);
+          return response.data.results.bindings;
+        })
+        .catch((error) => {
+          console.error(error);
+        }),
+      title: municipality,
+    });
 
-    // return data;
+    return data;
   }
 }
