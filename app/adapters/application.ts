e4d095bb068165272897a347611b8ac9cab19774
Denperidge <denperidge@gmail.com>
2023-04-24 18:02:58 +0200

    Changed API namespace

--- app/adapters/application.ts
+++ app/adapters/application.ts
@@ -3,6 +3,5 @@ import DS from "ember-data";
 export default DS.JSONAPIAdapter.extend({
   // host: "http://localhost:4200",
   // namespace: "api",
-  host: "https://burgernabije-besluitendatabank-dev.s.redhost.be",
-  namespace: "api",
+  host: "https://burgernabije-besluitendatabank-dev.s.redhost.be"
 });
