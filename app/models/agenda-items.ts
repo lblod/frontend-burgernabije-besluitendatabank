dffb20d2d9b2426db2cc5d05c16a1e88b6c97a10
Denperidge <denperidge@gmail.com>
2023-04-24 15:56:29 +0200

    Agenda-items (incompletely) through Ember-Data
    - Id & Title are loaded in correctly
    - Pagination is now broken (uh oh)
    - Other data & relationships are still missing

--- app/models/agenda-items.ts
+++ app/models/agenda-items.ts
@@ -3,11 +3,8 @@ import DS from "ember-data";
 
 export default class ItemsModel extends Model {
   @attr("string") declare title: string;
-  @attr("string") declare body: string;
-  @attr("date") declare approveddate: Date;
-  @attr("date") declare startdate: Date;
-  @attr("date") declare enddate: Date;
-  @attr("string") declare municipality: string;
+  @attr("string") declare alternateLink: string;
+  @attr("boolean") declare plannedForPublic : boolean;
 }
 
 // export default DS.Model.extend({
--- app/routes/home.ts
+++ app/routes/home.ts
@@ -22,6 +22,16 @@ export default class HomeRoute extends Route {
     // const endDate = params.endDate ? params.endDate : null;
 
     const municipalities = await getMunicipalitiesFromVlaanderen(true);
+    let agendaItems = await this.store.findAll("agenda-items");
+    // TODO let both find at the same time, also not findall bc jaysus
+    //let municipalities = await this.store.findAll("municipality)")
+
+    console.log(municipalities)
+    console.log(agendaItems)
+    return { 
+      agenda_items: agendaItems,
+      municipalities: municipalities
+    };
 
     // const resp = await this.store.findAll("agenda-items");
 
@@ -29,6 +39,7 @@ export default class HomeRoute extends Route {
     // return resp;
 
     // const resp = await
+    /*
     const resp = await Ember.RSVP.hash({
       agenda_items: axios
         .get(`${ENV.API_URL}/agenda-items?page[number]=${page}&page[size]=3`)
@@ -42,5 +53,6 @@ export default class HomeRoute extends Route {
     });
     console.log(resp);
     return resp;
+    */
   }
 }
