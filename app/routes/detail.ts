e6cc908e9e2fe299709cf528ca78ef86e1c8a2df
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-05-26 13:46:46 +0200

    feat(detail): Add voters names
    - switch detail to ember-data
    - add voters names in list
    
    related to #BNB-115

--- app/routes/detail.ts
+++ app/routes/detail.ts
@@ -1,33 +1,42 @@
 import Store from "@ember-data/store";
 import Route from "@ember/routing/route";
 import { service } from "@ember/service";
-import axios from "axios";
-import { getOneAgendaItemByTitle } from "frontend-burgernabije-besluitendatabank/utils/sparqlQueries";
 
 export default class DetailRoute extends Route {
   @service declare store: Store;
+
   async model(params: any) {
-    const { id } = params;
-    return await axios
-      .get(
-        "https://qa.centrale-vindplaats.lblod.info/sparql?query=" +
-          encodeURIComponent(
-            getOneAgendaItemByTitle({
-              title: id,
-            })
-          ),
-        {
-          headers: {
-            Accept: "application/sparql-results+json",
-          },
-        }
-      )
-      .then((response) => {
-        console.log(response.data.results.bindings[0]);
-        return response.data.results.bindings[0];
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    let agendaItems = await this.store.findRecord("agenda-item", params.id, {
+      include: [
+        // 'session',
+        // 'session.governing-body',
+        // 'session.governing-body.administrative-unit',
+        // 'handled-by',
+        'handled-by.has-votes',
+        // 'handled-by.has-votes.has-presents',
+        // 'handled-by.has-votes.has-abstainers',
+        'handled-by.has-votes.has-abstainers.alias',
+        // 'handled-by.has-votes.has-voters',
+        // 'handled-by.has-votes.has-opponents',
+        'handled-by.has-votes.has-opponents.alias',
+        // 'handled-by.has-votes.has-proponents',
+        'handled-by.has-votes.has-proponents.alias',
+      ].join(',')
+    });
+
+    // console.log(agendaItems);
+
+    return agendaItems;
+
+    // return this.store.query("agenda-item", 
+    // { 
+    //   filter: {
+    //     "title": params.title
+    //   },
+    //   include: 'session,session.governing-agent'
+    // }).then((agendaItem) => {
+    //   console.log(agendaItem);
+    //   return agendaItem.get("firstObject");
+    // });
   }
 }
