91b2e8ae839513151a2e7f4cffb159dfe8b49800
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-06-07 18:05:36 +0200

    feat(navigation): Add main menu

---
+++ app/components/navigation-bar/link.ts
@@ -0,0 +1,15 @@
+import Component from "@glimmer/component";
+import RouterService from "@ember/routing/router-service";
+import { service } from "@ember/service";
+
+interface ArgsInterface {
+    route: string;
+}
+
+export default class NavigationBar extends Component<ArgsInterface> {
+    @service declare router: RouterService;
+
+    get isActive() {
+        return this.router.currentRouteName.indexOf(this.args.route) !== -1;
+    }
+}
