bdc8cf25f038dba602f06cd1aef20ecc8c3df0e6
Denperidge <denperidge@gmail.com>
2023-06-23 16:43:53 +0200

    Fixed sessions.index load more filtering

--- app/controllers/sessions/index.ts
+++ app/controllers/sessions/index.ts
@@ -6,7 +6,7 @@ import SessionIndexRoute from '../../routes/sessions/index';
 import { tracked } from "@glimmer/tracking";
 import { service } from "@ember/service";
 import MunicipalityListService from "frontend-burgernabije-besluitendatabank/services/municipality-list";
-
+import { get } from "@ember/object";
 export default class SessionsIndexController extends Controller {
     declare model: ModelFrom<SessionIndexRoute>;
     @service declare store: Store;
@@ -19,12 +19,19 @@ export default class SessionsIndexController extends Controller {
 
     @action
     async loadMore() {
+        console.log(get(this.model, "municipality"));
+        console.log(get(this, "municipality"));
         //todo add max page guard
         if (this.model && !this.isLoadingMore) {
             this.isLoadingMore = true;
             const nextPage = this.model.currentPage + 1;
 
-            const sessions = await this.store.query("session", this.model.getQuery(nextPage));
+            let plannedStartMin = String(get(this, "plannedStartMin")) || undefined;
+            let plannedStartMax = String(get(this, "plannedStartMax")) || undefined;
+            let municipalities = String(get(this, "municipality")) || undefined;
+            console.log(plannedStartMax, plannedStartMax, municipalities)
+
+            const sessions = await this.store.query("session", this.model.getQuery(nextPage, plannedStartMin, plannedStartMax, municipalities));
             const concatenateSessions = this.model.sessions.concat(sessions.toArray());
             this.model.sessions.setObjects(concatenateSessions);
 
