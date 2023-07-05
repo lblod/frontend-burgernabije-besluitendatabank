ebb2056e6aab918e73227f8ed2250bf341aa5698
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-07-05 14:58:43 +0200

    Refactor: Extract `sortObjectsByTitle` in array-utils

--- app/controllers/sessions/session.ts
+++ app/controllers/sessions/session.ts
@@ -2,14 +2,13 @@ import Controller from "@ember/controller";
 import {computed} from "@ember/object";
 import { ModelFrom } from '../../lib/type-utils';
 import SessionSessionRoute from '../../routes/sessions/session';
+import { sortObjectsByTitle } from "frontend-burgernabije-besluitendatabank/utils/array-utils";
 
 export default class SessionsSessionController extends Controller {
     declare model: ModelFrom<SessionSessionRoute>;
 
     @computed('model.agendaItems')
     get agendaItemsSorted() {
-        return this.model.agendaItems.toArray().sort((a, b) => {
-            return a.title.localeCompare(b.title, undefined, { numeric: true });
-        });
+        return this.model.agendaItems.toArray().sort(sortObjectsByTitle);
     }
 }
---
+++ app/utils/array-utils.ts
@@ -0,0 +1,3 @@
+interface WithTitle { title: string }
+
+export const sortObjectsByTitle = (a: WithTitle, b: WithTitle) => a.title.localeCompare(b.title, undefined, { numeric: true });
