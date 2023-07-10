9df17546afe6fa0718afcf1609d645cb251ba0b1
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-07-10 16:52:57 +0200

    Fix some import paths
    In classic builds these default imports worked because of the AMD module compatibility layer. In Webpack based projects these imports are invalid, which result in runtime issues.

--- app/components/vote-overview.ts
+++ app/components/vote-overview.ts
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import d3 from 'd3';
+import * as d3 from 'd3';
 
 interface ArgsInterface {
   numberOfAbstentions: number;
