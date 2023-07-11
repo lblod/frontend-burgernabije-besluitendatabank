868e166104da6fb8d267205438b67023f390f590
Denperidge <denperidge@gmail.com>
2023-07-11 13:28:42 +0200

    Added an eslint ignore to hideDate

--- app/components/filters/date-range-filter.ts
+++ app/components/filters/date-range-filter.ts
@@ -1,23 +1,26 @@
 import { action } from '@ember/object';
 import FilterComponent from './filter';
-import { tracked } from "@glimmer/tracking";
+import { tracked } from '@glimmer/tracking';
 
 export default class SearcherDateRangeFilterComponent extends FilterComponent {
-  @tracked start: string|undefined = this.args.valueA ? this.args.valueA : undefined;
-  @tracked end: string|undefined = this.args.valueB ? this.args.valueB : undefined;
-
+  @tracked start: string | undefined = this.args.valueA
+    ? this.args.valueA
+    : undefined;
+  @tracked end: string | undefined = this.args.valueB
+    ? this.args.valueB
+    : undefined;
 
   changeValue(start?: string, end?: string) {
     this.start = start;
     this.end = end;
     this.updateQueryParams({
       [this.args.queryParamsA]: this.start,
-      [this.args.queryParamsB]: this.end
+      [this.args.queryParamsB]: this.end,
     });
   }
 
   @action
-  resetDate() { 
+  resetDate() {
     this.changeValue(undefined, undefined);
   }
 
@@ -25,7 +28,7 @@ export default class SearcherDateRangeFilterComponent extends FilterComponent {
   hideDate() {
     /**
      * There was a weird interaction with a weirder solution
-     * 
+     *
      * If you...
      * - Selected a daterange
      * - Applied
@@ -33,22 +36,24 @@ export default class SearcherDateRangeFilterComponent extends FilterComponent {
      * - Applied
      * - Opened
      * - And then hide it (don't select anything)
-     * 
+     *
      * It would display & apply the value you've used before resetting
-     * 
+     *
      * This was because the hideAction was the same as the applyAction
      * Not defining the hideAction makes au-date-range-picker throw an error
-     * 
+     *
      * So instead, an empty hideAction?
      * This kinda fixed it:
      * - Now the old value would no longer be applied
      * - But, the old value was still displayed
-     * 
+     *
      * So how do we fix it?
      * See the silly solution below
      */
+    /* eslint-disable no-self-assign */
     this.start = this.start;
     this.end = this.end;
+    /* eslint-enable no-self-assign*/
   }
 
   @action
