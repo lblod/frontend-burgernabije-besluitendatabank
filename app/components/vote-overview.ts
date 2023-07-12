db04179dc190c5752da23da558b6e3da05fedfe4
Dieter Peirs <github@dieterpeirs.com>
2023-07-12 15:00:55 +0200

    put magic number for stroke-dasharray in a variable

--- app/components/vote-overview.ts
+++ app/components/vote-overview.ts
@@ -21,33 +21,36 @@ export default class VoteOverview extends Component<ArgsInterface> {
   }
 
   get numberOfAbstentionsGraphValue() {
+    const strokeDashArray = 158;
     const totalValue =
       (this.args.numberOfProponents || 0) +
       (this.args.numberOfOpponents || 0) +
       (this.args.numberOfAbstentions || 0);
     const abstentionsValue = this.args.numberOfAbstentions || 0;
 
-    return (abstentionsValue / totalValue) * 158;
+    return (abstentionsValue / totalValue) * strokeDashArray;
   }
 
   get numberOfOpponentsGraphValue() {
+    const strokeDashArray = 158;
     const totalValue =
       (this.args.numberOfProponents || 0) +
       (this.args.numberOfOpponents || 0) +
       (this.args.numberOfAbstentions || 0);
     const opponentsValue = this.args.numberOfOpponents || 0;
 
-    return (opponentsValue / totalValue) * 158;
+    return (opponentsValue / totalValue) * strokeDashArray;
   }
 
   get numberOfProponentsGraphValue() {
+    const strokeDashArray = 158;
     const totalValue =
       (this.args.numberOfProponents || 0) +
       (this.args.numberOfOpponents || 0) +
       (this.args.numberOfAbstentions || 0);
     const proponentsValue = this.args.numberOfProponents || 0;
 
-    return (proponentsValue / totalValue) * 158;
+    return (proponentsValue / totalValue) * strokeDashArray;
   }
 
   @action
