b5677768f8981d9ae25ebb5245ac8d8d70e5c02b
Lenny <lennyb.0908@gmail.com>
2023-07-19 16:06:23 +0200

    remove includes from app/routes/detail, as they were breaking some agenda items + got bullied by linter

--- app/routes/detail.ts
+++ app/routes/detail.ts
@@ -16,19 +16,6 @@ interface FormattedTableVote {
   abstainer: MandataryModel | null;
 }
 
-const agendaItemIncludes = [
-  'handled-by.has-votes',
-  'handled-by.resolutions',
-  'handled-by.has-votes.has-abstainers.alias',
-  'handled-by.has-votes.has-abstainers.has-membership.inner-group',
-  'handled-by.has-votes.has-opponents.alias',
-  'handled-by.has-votes.has-opponents.has-membership.inner-group',
-  'handled-by.has-votes.has-proponents.alias',
-  'handled-by.has-votes.has-proponents.has-membership.inner-group',
-  'sessions.governing-body.is-time-specialization-of.administrative-unit.location',
-  'sessions.governing-body.administrative-unit.location',
-].join(',');
-
 export default class DetailRoute extends Route {
   @service declare store: Store;
   @service declare keywordStore: KeywordStoreService;
@@ -36,16 +23,12 @@ export default class DetailRoute extends Route {
   async model(params: DetailParams) {
     const agendaItem: AgendaItemModel = await this.store.findRecord(
       'agenda-item',
-      params.id,
-      {
-        include: agendaItemIncludes,
-      }
+      params.id
     );
 
     const sessionId = agendaItem.session?.id;
     const agendaItemOnSameSessionRaw = sessionId
       ? await this.store.query('agenda-item', {
-          include: agendaItemIncludes,
           filter: {
             sessions: {
               [':id:']: sessionId,
@@ -96,7 +79,6 @@ export default class DetailRoute extends Route {
         size: 4,
       },
       municipality: agendaItem.session?.municipality,
-      include: agendaItemIncludes,
       filter: {
         sessions: {
           'governing-body': {
