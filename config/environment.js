6def64d88b6ad9d8b91d346d77f52110cddb280b
Denperidge <denperidge@gmail.com>
2023-07-21 14:02:11 +0200

    Implemented plausible analytics

--- config/environment.js
+++ config/environment.js
@@ -5,9 +5,22 @@ module.exports = function (environment) {
     modulePrefix: 'frontend-burgernabije-besluitendatabank',
     environment,
     rootURL: '/',
-    locationType: 'history',
+    locationType: 'history', // If updated to hash, change ember-plausible config
     EmberENV: {
       EXTEND_PROTOTYPES: false,
+    },
+
+    // https://github.com/redpencilio/ember-plausible#configuration-options
+    'ember-plausible': {
+      enabled: true, // Unless specified otherwise (test env), enabled by default
+      apiHost: '{{PLAUSIBLE_APIHOST}}',
+      domain: '{{PLAUSIBLE_DOMAIN}}',
+
+      hashMode: false, // If locationType is hash, change to true
+      //trackLocalhost: true,  // Uncomment if you want your local instance to add to the statistics
+
+      enableAutoPageviewTracking: true, // if true, all page changes will send an event
+      enableAutoOutboundTracking: false, // if true, all clicks to external websites will send an event
     },
 
     APP: {
@@ -17,6 +30,8 @@ module.exports = function (environment) {
   };
 
   if (environment === 'test') {
+    ENV['ember-plausible'].enabled = false;
+
     ENV.locationType = 'none';
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
