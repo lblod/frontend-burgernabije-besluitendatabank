bac9e65a648e1a30c1c0cd33d60f4f0f5394fef9
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-07-26 13:37:16 +0200

    refactor: Remove unused imports

--- app/components/infinite-list.ts
+++ app/components/infinite-list.ts
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { throttle } from '@ember/runloop';
-import { tracked } from '@glimmer/tracking';
 
 interface ArgsInterface {
   loadMore: () => void;
