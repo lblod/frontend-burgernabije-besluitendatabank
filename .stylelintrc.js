01b628df3efdf07fa16ec94d5ca6a3ed80086cb8
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-08-14 14:15:28 +0200

    Set up Stylelint for Sass files
    This adds stylelint-config-standard-scss and resolves all linting issues.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-prettier/recommended'],
+  rules: {
+    'selector-class-pattern': null, // This enforces kebab-case by default but we use BEM which isn't compatible
+  },
 };
