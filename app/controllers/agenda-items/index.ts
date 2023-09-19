a06e093ba5efef690c58cd32f78bfcc7970ee51d
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-09-19 15:43:55 +0200

    feat(mu-search): Add `_exists_:abstract_location_id`

--- app/controllers/agenda-items/index.ts
+++ app/controllers/agenda-items/index.ts
@@ -200,8 +200,9 @@ const agendaItemsQuery = ({
   request.sort = '-session_planned_start';
   request.index = index;
 
-  // Ensure title fields are present
-  filters[':query:title'] = '_exists_:title';
+  // Ensure title and location_id fields are present
+  filters[':query:title'] =
+    '_exists_:title AND (_exists_:location_id OR _exists_:abstract_location_id)';
 
   // Apply optional filter for planned start range
   if (plannedStartMin) {
