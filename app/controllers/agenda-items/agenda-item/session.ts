b6f743a7be6ac168d5da61a6272fcf6ef13ef956
LBONTEN <lennyb.0908@gmail.com>
2023-09-21 14:43:01 +0200

    fixed: stop using agendaItemOnSameSession

--- app/controllers/agenda-items/agenda-item/session.ts
+++ app/controllers/agenda-items/agenda-item/session.ts
@@ -1,16 +1,17 @@
 import Controller from '@ember/controller';
-import { ModelFrom } from 'frontend-burgernabije-besluitendatabank/lib/type-utils';
-import AgendaItemsAgendaItemSessionRoute from 'frontend-burgernabije-besluitendatabank/routes/agenda-items/agenda-item/session';
+import { ModelFrom } from '../../../lib/type-utils';
 import { sortObjectsByTitle } from 'frontend-burgernabije-besluitendatabank/utils/array-utils';
+import AgendaItem from 'frontend-burgernabije-besluitendatabank/models/agenda-item';
+import AgendaItemsAgendaItemSessionRoute from 'frontend-burgernabije-besluitendatabank/routes/agenda-items/agenda-item/session';
 
 export default class AgendaItemsAgendaItemSessionController extends Controller {
   /** Used to fetch agenda items from the model */
   declare model: ModelFrom<AgendaItemsAgendaItemSessionRoute>;
 
   get agendaItemsSorted() {
-    let agendaItems = [this.model.agendaItem].concat(
-      this.model.agendaItemOnSameSession
-    );
+    let agendaItems = this.model.session.hasMany('agendaItems').value() as
+      | AgendaItem[]
+      | null;
 
     if (!agendaItems) {
       agendaItems = [];
