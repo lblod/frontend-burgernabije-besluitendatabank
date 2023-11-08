413214ef53026c36b384295ad06ce6449b97ee65
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-11-08 11:51:36 +0100

    feat(municipality-list): Deduplicate labels and remove Kruishoutem

--- app/services/municipality-list.ts
+++ app/services/municipality-list.ts
@@ -3,10 +3,13 @@ import Store from '@ember-data/store';
 import LocationModel from 'frontend-burgernabije-besluitendatabank/models/location';
 import { deserializeArray } from 'frontend-burgernabije-besluitendatabank/utils/query-params';
 
+type Municipality = { label: string; id: string };
+
 export default class MunicipalityListService extends Service {
   @service declare store: Store;
 
-  private _municipalities?: Array<{ label: string; id: string }>;
+  private _municipalities?: Array<Municipality>;
+  private _municipalityCleanedLabels?: Array<{ label: string }>;
 
   /**
    * Get all municipalities
@@ -22,6 +25,46 @@ export default class MunicipalityListService extends Service {
     }
 
     return this._municipalities;
+  }
+
+  /**
+   * Get all municipalities, filtered on the following criteria:
+   * - no duplicate labels
+   * - remove 'Kruishoutem' (because it's not a municipality anymore) #BNB-402
+   *
+   * Filtering is managed frontend as a temporary solution ^^
+   * TODO: move this to the backend
+   *
+   * @returns A promise for an array of municipality labels
+   **/
+
+  async municipalityLabels() {
+    if (!this._municipalityCleanedLabels) {
+      this._municipalityCleanedLabels = this._filteredMunicipalities(
+        await this.municipalities()
+      );
+    }
+
+    return this._municipalityCleanedLabels;
+  }
+
+  private _filteredMunicipalities(municipalities?: Array<Municipality>) {
+    if (!municipalities) {
+      return [];
+    }
+
+    const uniqueLabels = [
+      ...new Set(
+        municipalities
+          .filter(
+            (municipality) =>
+              municipality.label && municipality.label !== 'Kruishoutem'
+          )
+          .map(({ label }) => label)
+      ),
+    ].map((label) => ({ label }));
+
+    return uniqueLabels;
   }
 
   /**
