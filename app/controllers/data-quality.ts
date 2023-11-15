4ba2babf03fbedd6ff1a27916a4dbf35ed25e1e0
senne <sennebels@gmail.com>
2023-11-15 11:47:46 +0100

    refactor: :fire: removed comments and unused code

--- app/controllers/data-quality.ts
+++ app/controllers/data-quality.ts
@@ -3,25 +3,15 @@ import { action } from '@ember/object';
 import RouterService from '@ember/routing/router-service';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import AgendaItem from 'frontend-burgernabije-besluitendatabank/models/agenda-item';
 import FeaturesService from 'frontend-burgernabije-besluitendatabank/services/features';
 import MunicipalityListService from 'frontend-burgernabije-besluitendatabank/services/municipality-list';
 
 export default class DataQualityController extends Controller {
-  // add statistics that calculate the following:
-  // - total number of agenda items
-  // - total number of agenda items that have been treated
-  // - total number of agenda items that have a title and a description
-  // - total number of agenda items that don't have a title and a description
-  // - top 10 governing bodies with the most agenda items
-  // add a computed property that returns the percentage of agenda items that have been treated
-
   @service declare municipalityList: MunicipalityListService;
   @service declare router: RouterService;
   @tracked currentlyLoading = false;
   @service declare features: FeaturesService;
 
-  /** Controls the loading animation of the "locatie's opslaan" button */
   @tracked loading = false;
   @tracked selectedMunicipality: { label: string; id: string } | null = null;
 
@@ -33,7 +23,6 @@ export default class DataQualityController extends Controller {
     return this.features.isEnabled('statistics-page-feature');
   }
 
-  /** Resets the button loading animation */
   @action resetLoading() {
     this.loading = false;
   }
@@ -42,37 +31,11 @@ export default class DataQualityController extends Controller {
     label: string;
     id: string;
   }) {
-    // add query param for location ids
     this.selectedMunicipality = selectedMunicipality;
     this.router.transitionTo('data-quality', {
       queryParams: {
         gemeentes: selectedMunicipality ? selectedMunicipality.label : '',
       },
     });
-  }
-
-  get totalAgendaItems(): number {
-    const model = this.model as AgendaItem[];
-    return model.length;
-  }
-
-  get totalAgendaItemsTreated(): number {
-    const model = this.model as AgendaItem[];
-    return model.filter((item: AgendaItem) => item.handledBy).length;
-  }
-
-  get totalAgendaItemsWithTitleAndDescription(): number {
-    const model = this.model as AgendaItem[];
-    return model.filter((item: AgendaItem) => item.handledBy).length;
-  }
-
-  get totalAgendaItemsWithoutTitleAndDescription(): number {
-    const model = this.model as AgendaItem[];
-    return model.filter((item: AgendaItem) => item.handledBy).length;
-  }
-
-  get topTenGoverningBodies(): number {
-    const model = this.model as AgendaItem[];
-    return model.filter((item: AgendaItem) => item.handledBy).length;
   }
 }
