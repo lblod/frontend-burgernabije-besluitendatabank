df2d43f2c21cd0f6f92364b22680dce03335d851
senne <sennebels@gmail.com>
2023-12-22 11:04:44 +0100

    fix: :bug: added a guard on sortObjectsByTitle

--- app/utils/array-utils.ts
+++ app/utils/array-utils.ts
@@ -2,5 +2,9 @@ interface WithTitle {
   title: string;
 }
 
-export const sortObjectsByTitle = (a: WithTitle, b: WithTitle) =>
-  a.title.localeCompare(b.title, undefined, { numeric: true });
+export const sortObjectsByTitle = (a: WithTitle, b: WithTitle) => {
+  if (!a?.title || !b?.title) {
+    return a?.title ? -1 : 1;
+  }
+  return a?.title.localeCompare(b?.title, undefined, { numeric: true });
+};
