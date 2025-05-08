c7b5dc00c58236a5ccbd28febf39a52c78e493c8
Andres Vergauwen <andres.vergauwen@hotmail.com>
2025-05-08 13:48:05 +0200

    Redirect to citerra route

--- app/routes/application.ts
+++ app/routes/application.ts
@@ -1,16 +1,19 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import type RouterService from '@ember/routing/router-service';
 import type PlausibleService from 'ember-plausible/services/plausible';
 import config from 'frontend-burgernabije-besluitendatabank/config/environment';
 import type GoverningBodyDisabledList from 'frontend-burgernabije-besluitendatabank/services/governing-body-disabled-list';
 
 export default class ApplicationRoute extends Route {
+  @service declare router: RouterService;
   @service declare plausible: PlausibleService;
   @service declare governingBodyDisabledList: GoverningBodyDisabledList;
 
   beforeModel(): void {
     this.startAnalytics();
     this.setGoverningBodyDisabledList();
+    this.router.replaceWith('citerra.index'); // Remove this line if you want to keep the default behavior
   }
 
   startAnalytics(): void {
